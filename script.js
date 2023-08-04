8b8b1ba4dbaa5e85505904065855bee5dd776120
dihegocent <94179594+dihegocent@users.noreply.github.com>
2023-08-04 16:40:42 -0400

    Update script.js
    Fecha alert

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-    alert("Gracias por visitar mi portfolio, lastimosamente aún estoy trabajando en él, por favor ten paciencia :)");
+    alert("Gracias por visitar mi portfolio, lastimosamente aún estoy trabajando en él, por favor ten paciencia :) Incio del proyecto 02/08/2023");
 };
